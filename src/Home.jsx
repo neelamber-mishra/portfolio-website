84a25a67b3b456ff615b07b37e2b159a0ab2eeab
neelamber-mishra <24je0663@iitism.ac.in>
2025-04-17 01:10:57 +0530

    added a home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,23 @@
 function Home() {
-    let dev = "Neelamber Mishra";
+  let dev = "Neelamber Mishra";
 
-    return (
-        <div className="home">
-            <h1>{dev}</h1>
-            <p>
-            a computer science student at the indian institute of technology (dhanbad).
-            current fields of studying include machine learing , full-stack web-development, competetive coding and quant.
-            </p>
-        contact 
-        </div>
-    );
+  return (
+    <div className="home" id="home">
+      <h1>web-developer</h1>
+      <p className="links">
+        say hallo <i className="fas fa-coffee"></i>
+      </p>
+      <p className="intro">
+        Hello i am {dev} , a computer science student at the indian institute of
+        technology (dhanbad). current fields of studying include machine learing
+        , full-stack web-development, competetive coding and quant.
+      </p>
 
+      <a href="#">
+        <button>Contact</button>
+      </a>
+    </div>
+  );
 }
 
 export default Home;
