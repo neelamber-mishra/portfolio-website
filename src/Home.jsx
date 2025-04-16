8053a216082a4ac271e43f78309e429b29d49d04
neelamber-mishra <24je0663@iitism.ac.in>
2025-04-17 01:13:08 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
         say hello <i className="fas fa-coffee"></i>
       </p>
       <p className="intro">
-        Hello i am {dev} , a computer science student at the indian institute of
-        technology (dhanbad). current fields of studying include machine learning
+        Hello i am {dev} , a computer science student at the Indian Institute of
+        technology (IIT dhanbad). current fields of studying include machine learning
         , full-stack web-development, competetive coding and quant.
       </p>
 
