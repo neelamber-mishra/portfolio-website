5b8117749855f69a3506a02f73baee7125c0e62d
neelamber-mishra <24je0663@iitism.ac.in>
2025-05-11 03:17:32 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function About() {
   const [dev, setDev] = useState("Neelamber Mishra");
   const [intro, setIntro] = useState(
-    "Hello I am Neelamber Mishra, a computer science student atompetitive coding and quant."
+    ""
   );
 
   return (
