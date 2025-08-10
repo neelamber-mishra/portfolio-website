24cac44342f9ad21ee6d7adb5f9e340e8d0df9e0
neelamber-mishra <24je0663@iitism.ac.in>
2025-08-10 11:27:33 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,21 +7,32 @@ interface RepoStats {
 }
 
 const Footer = async () => {
-    const repoStats = await fetch(
+    const repoStats: RepoStats = await fetch(
         'https://api.github.com/repos/neelamber-mishra/portfolio-2.0',
         {
             next: {
                 revalidate: 60 * 60, // 1 hour
             },
-        },
-    );
+        }
+    ).then(res => res.json());
 
-    const { stargazers_count, forks_count } =
-        (await repoStats.json()) as RepoStats;
+    const { stargazers_count, forks_count } = repoStats;
 
     return (
         <footer className="text-center pb-5" id="contact">
             <div className="container">
+                {/* GitHub Stats */}
+                <div className="flex justify-center gap-4 mb-4">
+                    <div className="flex items-center gap-1">
+                        <Star className="w-4 h-4" />
+                        <span>{stargazers_count}</span>
+                    </div>
+                    <div className="flex items-center gap-1">
+                        <GitFork className="w-4 h-4" />
+                        <span>{forks_count}</span>
+                    </div>
+                </div>
+
                 <p className="text-lg">Have a project in mind?</p>
                 <a
                     href={`mailto:${GENERAL_INFO.email}`}
@@ -29,7 +40,6 @@ const Footer = async () => {
                 >
                     {GENERAL_INFO.email}
                 </a>
-
             </div>
         </footer>
     );
